5237429c78a8629950a1387b9d9d4a72c9917ca5
lumi g <noahgueye19@gmail.com>
2024-06-05 19:02:15 +0100

    first commit

--- Communication/settings.py
+++ Communication/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+l*fu!asqw6wmomwy$@0^!um4tcnej5yz(-r2o==%w01)v!yro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
